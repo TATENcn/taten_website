896e72b3db6e80243622b1e12e761bf404f20bc8
Overflow_Error <linmohan0814@163.com>
2025-10-24 19:07:16 +0800

    🐞 fix:修正团队说明

--- server/api/typing-text.get.ts
+++ server/api/typing-text.get.ts
@@ -1,4 +1,4 @@
-import { defineEventHandler } from 'h3'
+import { defineEventHandler } from "h3";
 
 export default defineEventHandler((): string[] => {
   return [
@@ -6,4 +6,4 @@ export default defineEventHandler((): string[] => {
     "一群热爱编程的学生",
     "在网络的另一端，我们相遇；在代码与梦想之间，我们并肩前行",
   ];
-})
+});
